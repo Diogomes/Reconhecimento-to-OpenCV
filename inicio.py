7ae930f719bd769452563dbce761ffa8d71be5fb
Diogo <40742985+Diogomes@users.noreply.github.com>
2021-04-08 08:55:14 -0300

    commit mudanças

--- inicio.py
+++ inicio.py
@@ -7,8 +7,10 @@ imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('elon2.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-#faceLoc = face_recognition.face_locations()
-
+#faceLoc = face_recognition.face_locations(imgElon)[0]
+#encodeElon = face_recognition.face_encodings(imgElon)[0]
+#cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[2]),(255,0,255),2)
+''
 cv2.imshow('elon1',imgElon)
 cv2.imshow('elon2',imgTest)
 cv2.waitKey(0)
